b443d859fe0103415122a9611b8abc4058f07f84
Maxime <maxime.husson@hotmail.fr>
2020-02-01 14:41:33 +0100

    First Push

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    //Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scale.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float min;
+
+    Vector3 temp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temp = transform.localScale;
+
+        temp.x -= Time.deltaTime;
+
+        temp.y -= Time.deltaTime;
+
+         if(temp.x <= 0 || temp.y <= 0)
+        {
+            Debug.Log("game over");
+        }
+
+        transform.localScale = temp;
+
+       // if  (transform.localScale)
+            
+
+           
+
+        
+
+    }
+}
